e45af630053572b8ea6af4428ac459d778952e65
Tolu Olubode <toluwanimiolubode@gmail.com>
2018-11-24 15:18:15 -0500

    updates to the front page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'HealthChat',
+    title: 'HealthMate',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
+import { Button } from 'semantic-ui-react'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+import Layout from '../components/layout';
+
+import '../pages/index.css';
 
 import 'semantic-ui-css/semantic.min.css';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Welcome to HealthChat</h1>
-    <p>Reducing waittimes by connecting patients to qualified healthcare practitioners before the ER.</p>
+    <div className="hero-text">
+      <h1>Welcome to HealthMate</h1>
+      <p>Reducing waittimes by connecting patients to qualified healthcare practitioners before the ER.</p>
+    </div>
+    <div className="formstyle">
+      <label className="form-label" for="username">Username</label>
+      <input className="form-input" type="text" name="username"/>
+      <br></br>
+      <label className="form-label" for="password">Password</label>
+     <input className="form-input" type="password" name="password"/>
+     <br></br>
+     <Button color='orange' className="Signupbtn">Sign In</Button>
+    </div>
   </Layout>
 )
 
